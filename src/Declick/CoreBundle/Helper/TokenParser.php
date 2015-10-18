eb436274f29fa6284ce68ba4d79c779309330b37
bonpra <benoit@192.168.0.11>
2015-10-18 21:17:38 +0200

    Added ability to parse token coming from france ioi platform

---
+++ src/Declick/CoreBundle/Helper/TokenParser.php
@@ -0,0 +1,43 @@
+<?php
+namespace Declick\CoreBundle\Helper;
+
+use Namshi\JOSE\JWS;
+use DateTime;
+
+class TokenParser
+{
+   /**
+    * Public key
+    */
+   private $publicKey;
+   
+   function __construct($publicKey) {
+      $this->publicKey = openssl_pkey_get_public("file://".$publicKey);
+   }
+   /**
+    * Decode tokens
+    */
+   public function decodeToken($encryptedParams)
+   {
+      $jws  = JWS::load($encryptedParams);;
+      if ($jws->verify($this->publicKey)) {
+          $params = $jws->getPayload();
+      }
+      $datetime = new DateTime();
+      $datetime->modify('+1 day');
+      $tomorrow = $datetime->format('d-m-Y');
+      if (!isset($params['date'])) {
+         if (!$params) {
+            throw new Exception('Token cannot be decrypted, please check your SSL keys');
+         }
+         else {
+            throw new Exception('Invalid Task token, unable to decrypt: '.$params.'; current: '.date('d-m-Y'));
+         }
+      }
+      else if ($params['date'] != date('d-m-Y') && $params['date'] != $tomorrow) {
+         throw new Exception('API token expired');
+      }
+      
+      return $params;
+   }
+}
